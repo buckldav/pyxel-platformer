d54176b4936f110a134938c14f2034b77a6e0ad8
David Buckley <runalldayandnight@gmail.com>
2023-02-02 19:32:58 -0700

    poor knockback

---
+++ engine.py
@@ -0,0 +1,95 @@
+import pyxel
+from typing import Optional, List, Callable
+
+GRAVITY = 0.5
+TERMINAL_VELOCITY = 8
+PLAYER_SPEED = 4
+
+
+class Physics:
+    def __init__(self):
+        self.dy = 0
+        self.is_grounded = False
+
+    def fall(self, is_colliding=False):
+        if not is_colliding:
+            self.dy += GRAVITY
+            if self.dy > TERMINAL_VELOCITY:
+                self.dy = TERMINAL_VELOCITY
+        else:
+            self.dy = 0
+            self.is_grounded = True
+
+        if self.dy > GRAVITY:
+            self.is_grounded = False
+
+
+class BoxWithCollision:
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        w: float,
+        h: float,
+        col: int,
+        filled=False,
+        phys=False,
+        keys_move_x_pos: List[int] = [],
+        keys_move_x_neg: List[int] = [],
+        keys_jump: List[int] = [],
+    ):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.col = col
+        self.filled = filled
+        self.phys = Physics() if phys else None
+        self.keys_move_x_pos = keys_move_x_pos
+        self.keys_move_x_neg = keys_move_x_neg
+        self.keys_jump = keys_jump
+
+    def is_colliding_top(self, box):
+        if (box.y + box.h >= self.y and box.y <= self.y) and (
+            (self.x <= box.x + box.w and self.x + self.w >= box.x + box.w)
+            or (self.x <= box.x and self.x + self.w >= box.x)
+        ):
+            # knockback
+            box.y = self.y - box.h - GRAVITY
+            return True
+        return False
+
+    def fall(self, collider: Callable[[any], bool]):
+        if self.phys:
+            # call the collider to see if the object can fall
+            self.phys.fall(collider(self))
+            self.y += self.phys.dy
+            # call the collider a second time for knockback
+            collider(self)
+
+    def inputs(self):
+        for key in self.keys_move_x_pos:
+            # move x positive
+            if pyxel.btn(key):
+                self.x += PLAYER_SPEED
+                break
+
+        for key in self.keys_move_x_neg:
+            # move x negative
+            if pyxel.btn(key):
+                self.x -= PLAYER_SPEED
+                break
+
+        for key in self.keys_jump:
+            # jump
+            if pyxel.btnp(key) and self.phys and self.phys.is_grounded:
+                self.phys.is_grounded = False
+                self.phys.dy = -10
+                self.y += self.phys.dy
+                break
+
+    def draw(self):
+        if self.filled:
+            pyxel.rect(self.x, self.y, self.w, self.h, self.col)
+        else:
+            pyxel.rectb(self.x, self.y, self.w, self.h, self.col)
---
+++ game.py
@@ -0,0 +1,45 @@
+import pyxel
+from engine import *
+
+SKY_COLOR = 6
+PLAYER_COLOR = 8
+FLOOR_COLOR = 4
+
+class App:
+    def __init__(self):
+        pyxel.init(400, 300, title="Platformer", quit_key=pyxel.KEY_Q)
+        self.floors = [
+            BoxWithCollision(
+                0, pyxel.height - 20, pyxel.width, 20, FLOOR_COLOR, filled=True
+            ),
+            BoxWithCollision(50, pyxel.height - 100, 40, 20, FLOOR_COLOR, filled=True),
+        ]
+        self.player = BoxWithCollision(
+            20,
+            20,
+            20,
+            20,
+            PLAYER_COLOR,
+            filled=True,
+            phys=True,
+            keys_move_x_pos=[pyxel.KEY_D, pyxel.KEY_RIGHT],
+            keys_move_x_neg=[pyxel.KEY_A, pyxel.KEY_LEFT],
+            keys_jump=[pyxel.KEY_W, pyxel.KEY_SPACE, pyxel.KEY_UP],
+        )
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        for floor in self.floors:
+            self.player.fall(floor.is_colliding_top)
+        self.player.inputs()
+        pyxel.camera(self.player.x - 50, 0)
+
+    def draw(self):
+        pyxel.cls(SKY_COLOR)
+
+        for floor in self.floors:
+            floor.draw()
+        self.player.draw()
+
+
+App()
